7fc2f7c310b1d11ddb7fc4203e9713516a2b6ef4
nikdvb <nikdvb@gmail.com>
2021-08-26 01:28:44 -0400

    fix config + intro text

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -19,7 +19,7 @@ class Intro extends Component {
                     <div className="description">
                         <p>
                             Hi, my name is Nikolas, I'm a Full-Stack developer who's interested in all sorts of web technologies, 
-                            from the frontend to the backend. I'm a proud software engineering who loves designing and developing new and exciting 
+                            from the frontend to the backend. I'm a proud software engineer who loves designing and developing new and exciting 
                             software whether it's an application API, development tools, or adapting UI designs for web applications.
                         </p>
                     </div>
