fdb0330a5707926d1cd15aa37ebae71abd9fc078
nikdvb <nikdvb@gmail.com>
2021-11-17 19:56:02 -0500

    update projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import '../styles/components/Projects.scss';
-import meteorLogo from '../assets/images/meteor-logo.svg';
 import mongodbLogo from '../assets/images/mongodb.svg';
 import reactjsLogo from '../assets/images/reactjs-icon.svg';
 import sassLogo from '../assets/images/sass.svg';
@@ -24,19 +23,45 @@ class Projects extends Component {
         <div className="projects">
             <h2>Projects</h2>
             <div className="project">
-                <h3>United Airlines Microsite</h3>
+                <h3>Airtime Portal</h3>
                 <div className="info">
                     <p className="description">
-                        A CMS driven website that displays upcoming entertainment in passenger flights. 
-                        The project was built using Meteor as an SPA controller, React.js and Sass for the UI, 
-                        and Mongodb as the data model. Tasks involved implementing designs, setting up a database 
-                        driven by a CMS, and ensuring that accessibility was in accordance with W3C accessibility 
-                        guidelines.
+                        Contributed to both the frontend and backend development of this generic and themeable application that offers content such as
+                        movies, tv series, audio, games, wifi connectivity, and flight information to passengers in-flight. The frontend project uses 
+                        AngularJs as the main framework, RxJS to fetch data from our backend API, Sass for styling and theming, Cypress for testing
+                        , and Gulp and Jenkins for build and testing automation. For the backend, there are two Falcon python APIs, the on-ground API, 
+                        which is hosted on Ubuntu servers and deployed using Docker and Ansible, and there is the in-flight API, which lives on Archlinux 
+                        servers on the plane and is installed via pacman packages. Notably, I worked on adding the games functionality to the frontend 
+                        application, and I also worked on the backend software design and implementation of the in-flight shopping and Apple Pay features.
                     </p>
+                    <div className="links">
+                        <a href="https://www.anuvu.com/our-portfolio/products#showcase-intro-airtime-portal" target="_blank">Find Out More</a>
+                    </div>
                     <div className="stack">
-                        <h4>Technology Stack</h4>
                         <div className="svgs">
-                            <img src={meteorLogo}/>
+                            <img src={angularLogo}/>
+                            <img src={reactjsLogo}/>
+                            <img src={sassLogo}/>
+                            <img src={mongodbLogo}/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="project">
+                <h3>Microportal</h3>
+                <div className="info">
+                    <p className="description">
+                        Contributed to the development of this generic and themeable template for CMS driven websites that display upcoming entertainment 
+                        in passenger flights. The project was built using AngularJs to create an SPA, RxJS to fetch data from the CMS, Less for styling 
+                        and theming, Gulp to automate the build and theming process, and Cypress, Jasmine and Karma for testing. Tasks involved implementing 
+                        designs, and ensuring that accessibility was in accordance with W3C accessibility guidelines.
+                    </p>
+                    <div className="links">
+                        <a href="https://entertainment.cathaypacific.com/" target="_blank">Visit the Website</a>
+                    </div>
+                    <div className="stack">
+                        <div className="svgs">
+                            <img src={angularLogo}/>
                             <img src={reactjsLogo}/>
                             <img src={sassLogo}/>
                             <img src={mongodbLogo}/>
@@ -55,7 +80,6 @@ class Projects extends Component {
                     Spotify API. 
                     </p>
                     <div className="stack">
-                        <h4>Technology Stack</h4>
                         <div className="svgs">
                             <img src={phonegapLogo}/>
                             <img src={angularLogo}/>
@@ -68,15 +92,18 @@ class Projects extends Component {
                 </div>
             </div>
             <div className="project">
-                <h3>Artist Album Page</h3>
+                <h3>Album Page</h3>
                 <div className="info">
                     <p className="description">
                         Simple web page with custom audio player and 3d rotatable album hosted on a free heroku 
                         instance with custom domain made for an album I composed and produced in 2019. Created 
-                        using HTML, CSS, JS and p5.js for the 3d album.
+                        using HTML, CSS, JS and p5.js for the 3d album. (The link may take time to load since 
+                        the heroku server may be asleep).
                     </p>
+                    <div className="links">
+                        <a href="http://sequester.xyz/index.html" target="_blank">Visit the Website</a>
+                    </div>
                     <div className="stack">
-                        <h4>Technology Stack</h4>
                         <div className="svgs">
                             <img src={herokuLogo}/>
                             <img src={htmlLogo}/>
