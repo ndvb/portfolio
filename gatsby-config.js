7b7b3344d04d1ee3b40ede744364bc57c42cd6bc
nikdvb <nikdvb@gmail.com>
2021-11-21 18:07:22 -0500

    fix breakpoints

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,8 +15,6 @@ module.exports = {
         path: `${__dirname}/src/assets/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
